e0af8f37cc4e0173d9ea090c2bb25023584e2983
merunga <merunga@gmail.com>
2018-03-08 08:06:27 -0500

    initial todos en step5

--- step5_reducers/src/reducers/todos.js
+++ step5_reducers/src/reducers/todos.js
@@ -1,4 +1,12 @@
-const todos = (state = [], action) => {
+const initialTodos = [
+  { id:1, text: 'Trepar a un arbol', completed: false },
+  { id:2, text: 'Ver un ovni', completed: true },
+  { id:3, text: 'Aprender a cultivar tomates', completed: false },
+  { id:4, text: 'Hornear pan', completed: false },
+  { id:5, text: 'Saltar en paracaidas', completed: true },
+]
+
+const todos = (state = initialTodos, action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
@@ -11,7 +19,7 @@ const todos = (state = [], action) => {
       ]
     case 'TOGGLE_TODO':
       return state.map(todo =>
-        (todo.id === action.id) 
+        (todo.id === action.id)
           ? {...todo, completed: !todo.completed}
           : todo
       )
