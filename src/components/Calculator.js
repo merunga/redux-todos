079ab176fb2e2efeaa30a786e6d6584a152e59e3
Manuela <fmanuela499@gmail.com>
2018-03-12 04:23:43 -0500

    calculator.js 2da parte

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,41 +2,69 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./Calculator.css";
 import { operators } from "../operators";
-import {
-      enterNumber
-    , setOperator
-    , clear
-    , evaluate
-} from "../ducks/calculator";
+import { enterNumber, setOperator, clear, evaluate } from "../ducks/calculator";
 import CalculatorButton from "./CalculatorButton/CalculatorButton";
 import Display from "./Display/Display";
 export class Calculator extends Component {
-    render() {
-        const {
-              currentValue, enterNumber, setOperator, clear, evaluate
-        } = this.props;
-        const numberButtons = [ 7, 8, 9, 4, 5, 6, 1, 2, 3, 0 ].map( ( number ) => (
+  render() {
+    const {
+      currentValue,
+      enterNumber,
+      setOperator,
+      clear,
+      evaluate
+    } = this.props;
+    const numberButtons = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0].map(number => (
+      <CalculatorButton
+        myFunction={() => enterNumber(number)}
+        key={number}
+        value={number}
+      />
+    ));
+    return (
+      <main className="calculator">
+        <Display value={currentValue} />
+        <div className="">
+          <section className="">
+            <CalculatorButton myFunction={clear} value="AC" />
+            {numberButtons}
+            <CalculatorButton myFunction={() => enterNumber(".")} value="." />
+          </section>
+          <section className="">
             <CalculatorButton
-                myFunction={ () => enterNumber( number ) }
-                key={ number }
-                value={ number }
-                
+              myFunction={() => setOperator(operators.DIVIDE)}
+              value="÷"
             />
-        ) );
-        return (
-            <main className="calculator">
-                <Display value={ currentValue } />
-                <div className="">
-                    <section className="">
-                        <CalculatorButton
-                            myFunction={clear}
-                            value="AC"
-                        />
-                        { numberButtons }
-                       ---
-                    </section>
-                </div>
-            </main>
-        );
-    }
+            <CalculatorButton
+              myFunction={() => setOperator(operators.MULTIPLY)}
+              value="×"
+            />
+            <CalculatorButton
+              myFunction={() => setOperator(operators.SUBTRACT)}
+              value="-"
+            />
+            <CalculatorButton
+              myFunction={() => setOperator(operators.ADD)}
+              value="+"
+            />
+            <CalculatorButton
+              className="equal"
+              myFunction={evaluate}
+              value="="
+            />
+          </section>
+        </div>
+      </main>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  return state;
+}
+export default connect(mapStateToProps, {
+  enterNumber,
+  setOperator,
+  clear,
+  evaluate
+})(Calculator);
